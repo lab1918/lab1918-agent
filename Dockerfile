7218846fb8930c5aaec17569801e3530fc278f8f
Eric Dong <igp2bgp@gmail.com>
2024-09-28 19:45:37 -0700

    refactor the code

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ FROM python:3.9
 RUN mkdir -p /home/lab1918
 WORKDIR /home/lab1918
 
-RUN apt-get update && apt-get install -y jq less awscli
-
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY src/ /home/lab1918
 
-CMD celery -A lab1918_agent.deployer worker --pool=prefork --concurrency=4 --loglevel=info -E
+CMD PYTHONPATH=. python3 lab1918_agent/deployer.py
--- src/lab1918_agent/deployer.py
+++ src/lab1918_agent/deployer.py
@@ -1,5 +1,8 @@
-from lab1918_agent.utils import logger
-from lab1918_agent.celery import get_celery_app
+import logging
+import os
+
+from lab1918_agent.assume_role import assume_role
+from lab1918_agent.utils import logger, get_celery_app
 
 
 app = get_celery_app()
@@ -11,3 +14,21 @@ def ping(self, *args, **kwargs) -> str:
         f"Running task ping, id: {self.request.id}, args: {args}, kwargs: {kwargs}"
     )
     return "pong"
+
+
+def setup_env():
+    role = os.getenv("STS_ROLE_ARN")
+    if role is None:
+        logger.info(f"no role arn, skip setup env")
+        return
+
+    for k, v in assume_role().items():
+        os.environ[k] = v
+    logger.info(f"switch role to {role}")
+
+
+if __name__ == "__main__":
+    setup_env()
+    worker = app.Worker(include=["lab1918_agent.deployer"])
+    worker.setup_defaults(concurrency=4, loglevel=logging.INFO)
+    worker.start()
