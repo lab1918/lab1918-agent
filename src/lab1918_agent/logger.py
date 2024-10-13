6f7cddc468203ed5934f0fffc02a270f0f0fced7
Eric Dong <igp2bgp@gmail.com>
2024-10-13 14:50:50 -0700

    fix double logging issue

--- src/lab1918_agent/logger.py
+++ src/lab1918_agent/logger.py
@@ -1,8 +1,4 @@
 import logging
 
 logger = logging.getLogger("agent")
-formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] %(message)s")
-handler = logging.StreamHandler()
 logger.setLevel(logging.INFO)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
