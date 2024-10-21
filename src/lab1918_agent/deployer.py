9883948ea3e48153b5af35c1bcaaef5c0f6960b4
Eric Dong <igp2bgp@gmail.com>
2024-10-20 19:48:11 -0700

    add delete container and delete network task

--- src/lab1918_agent/deployer.py
+++ src/lab1918_agent/deployer.py
@@ -32,6 +32,17 @@ def create_container(self, container_name, image_name, networks) -> bool:
 
 
 @app.task(bind=True)
+def delete_container(self, container_name) -> bool:
+    logger.info(
+        f"Running task delete_container, id: {self.request.id}, container_name: {container_name}"
+    )
+    client = ContainerClient()
+    client.delete_container(container_name)
+    logger.info(f"deleted container {container_name}")
+    return True
+
+
+@app.task(bind=True)
 def create_network(self, network_name, network_driver) -> bool:
     logger.info(
         f"Running task create_network, id: {self.request.id}, network_name: {network_name}, network_driver: {network_driver}"
@@ -39,4 +50,15 @@ def create_network(self, network_name, network_driver) -> bool:
     client = ContainerClient()
     client.create_network(network_name, network_driver)
     logger.info(f"created network {network_name}")
+    return True
+
+
+@app.task(bind=True)
+def delete_network(self, network_name) -> bool:
+    logger.info(
+        f"Running task delete_network, id: {self.request.id}, network_name: {network_name}"
+    )
+    client = ContainerClient()
+    client.delete_network(network_name)
+    logger.info(f"deleted network {network_name}")
     return True
